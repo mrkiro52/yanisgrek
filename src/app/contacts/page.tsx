164a7bbf09e24f863ecbab12b093c151cb346ab2
Данил Страшный <danilstrasnyj@MacBook-Air-Danil.local>
2025-06-17 01:58:44 +0400

    Contact_Desktop_Push

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,95 @@
+"use client";
+import "./contacts.scss";
+
+export default function Contacts() {
+  return (
+    <div className="Contacts">
+      <div className="Bg_contacts" />
+      <div className="right-blur-overlay" />
+      <div className="ContactFormSection">
+        <h2>
+          Запишитесь на диагностику, <br />
+          Выбрав день и время
+        </h2>
+
+        <form className="form">
+          <div className="form-row">
+            <label htmlFor="name" className="form-label">
+              Ваше имя
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="form-input"
+              placeholder="Иванов Иван"
+            />
+          </div>
+
+          <div className="form-row">
+            <label htmlFor="phone" className="form-label">
+              Ваш телефон
+            </label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              className="form-input"
+              placeholder="+7 (9xx) xxx xx xx"
+            />
+          </div>
+
+          <div className="form-row">
+            <label htmlFor="email" className="form-label">
+              Ваша почта
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="form-input"
+              placeholder="yanis_grek@gmail.com"
+            />
+          </div>
+
+          <div className="form-row">
+            <label htmlFor="date" className="form-label">
+              Дата
+            </label>
+            <div className="form-datetime">
+              <input type="date" id="date" name="date" className="form-input" />
+              <input type="time" id="time" name="time" className="form-input" />
+            </div>
+          </div>
+
+          <button type="submit" className="form-button">
+            Записаться
+          </button>
+        </form>
+
+        {/* Контактная информация */}
+        <div className="contacts-info">
+          <div className="contacts-info-block">
+            <h2 className="title-contacts-info">Москва</h2>
+            <p>
+              г.Москва <br />
+              Высоковольтный проезд, дом 1 стр. 29
+            </p>
+          </div>
+          <div className="contacts-info-block">
+            <h2 className="title-contacts-info">Режим работы:</h2>
+            <p>
+              пн.- пт. с 10:00 до 20:00 <br />
+              сб. с 11:00 до 20:00 <br />
+              вс. с 11:00 до 18:00 <br />
+            </p>
+          </div>
+          <div className="contacts-info-block">
+            <h2 className="title-contacts-info">Телефоны:</h2>
+            <p>+7 (495) 76-76-500</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
