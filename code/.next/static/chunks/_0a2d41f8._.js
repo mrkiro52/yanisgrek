7ef127897fd8e8906929c296c9e7fc8d839732cc
penis@gmail.com <mrkr@MacBook-Air-Hanil.local>
2025-07-13 18:45:40 +0400

    images slider

--- code/.next/static/chunks/_0a2d41f8._.js
+++ code/.next/static/chunks/_0a2d41f8._.js
@@ -1192,7 +1192,7 @@ function AboutPage() {
                                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("h1", {
                                     className: "title_block_about",
                                     children: [
-                                        "О нас — автосервис в Москве",
+                                        "О нас — автосервис YANIS GREK в Москве",
                                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("br", {}, void 0, false, {
                                             fileName: "[project]/src/app/about/page.tsx",
                                             lineNumber: 55,
