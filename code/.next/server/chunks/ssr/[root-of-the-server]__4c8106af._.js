9dc161b59dfb02ee2a23464cbd65fe96e62ea3a5
penis@gmail.com <mrkr@MacBook-Air-Hanil.local>
2025-07-15 17:26:21 +0400

    optimized with json

--- code/.next/server/chunks/ssr/[root-of-the-server]__4c8106af._.js
+++ code/.next/server/chunks/ssr/[root-of-the-server]__4c8106af._.js
@@ -364,9 +364,14 @@ async function CarPage({ params }) {
                 lineNumber: 73,
                 columnNumber: 7
             }, this),
+            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$components$2f$Calculator$2f$Calculator$2e$jsx__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"], {}, void 0, false, {
+                fileName: "[project]/src/app/cars/[model]/page.jsx",
+                lineNumber: 77,
+                columnNumber: 7
+            }, this),
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$contacts$2f$page$2e$jsx__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"], {}, void 0, false, {
                 fileName: "[project]/src/app/cars/[model]/page.jsx",
-                lineNumber: 76,
+                lineNumber: 78,
                 columnNumber: 7
             }, this)
         ]
