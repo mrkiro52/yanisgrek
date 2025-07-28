f132429c246c84ae0cf8418066d3bb0f1741f41d
penis@gmail.com <mrkr@somename.local>
2025-07-28 12:57:20 +0300

    some scss

--- code/.next/static/chunks/src_app_about_page_tsx_1cfda39e._.js
+++ code/.next/static/chunks/src_app_about_page_tsx_1cfda39e._.js
@@ -2,8 +2,8 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/node_modules_96bf70e9._.js",
-  "static/chunks/src_42e11024._.js",
-  "static/chunks/src_aae184cd._.css"
+  "static/chunks/src_3b124a34._.js",
+  "static/chunks/src_1e34e92c._.css"
 ],
     source: "dynamic"
 });
