0b74c29693e7fba4bf6a970a42ed6444a0740d4c
penis@gmail.com <mrkr@somename.local>
2025-07-28 14:00:02 +0300

    fixes form etc

--- code/.next/static/chunks/src_app_page_jsx_1cfda39e._.js
+++ code/.next/static/chunks/src_app_page_jsx_1cfda39e._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_next_4533e4d1._.js",
   "static/chunks/src_6a43cd7a._.js",
+  "static/chunks/node_modules_next_4533e4d1._.js",
   "static/chunks/src_a111ab4a._.css"
 ],
     source: "dynamic"
