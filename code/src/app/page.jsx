a3b44b050776b5d34d3f0e151177461147e6d303
Name <example@gmail.com>
2025-10-15 09:36:42 +0700

    reviewlab

--- code/src/app/page.jsx
+++ code/src/app/page.jsx
@@ -73,7 +73,7 @@ export default async function Home() {
       <Quiz/>
       <Discounts />
       <div style={{paddingTop: "130px"}}></div>
-      <review-lab data-widgetid="68adc1e5122d897a4957fded"></review-lab>
+      <review-lab data-widgetid="68ef05ca67defc8041d3ab95"></review-lab>
       <script src="https://app.reviewlab.ru/widget/index-es2015.js" defer></script>
 
       <div className="blackInfo">
